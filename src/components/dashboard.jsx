d6dfe0855c7d68836485d978840e27ccef8b52f9
Bayram Çınar <bayram200212@gmail.com>
2023-12-26 21:27:22 +0300

    update

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard() {
         </div>
         <div className="flex max-[768px]:block">
           <div className="flex justify-start bg-dayComponentBg pb-8 w-[100%]">
-            <div className="w-[750px] ml-[9rem] max-[768px]:ml-[1.5rem] max-[768px]:w-[24rem] bg-dayComponentBg setAppointmentTime flex flex-col shadow-xl border-stepBorder1 border-2 rounded-xl">
+            <div className="w-[750px] ml-[9rem] max-[768px]:mx-auto max-[768px]:w-[24rem] bg-dayComponentBg setAppointmentTime flex flex-col shadow-xl border-stepBorder1 border-2 rounded-xl">
               <h1 className="text-buttonColor text-2xl text-center font-semibold mt-5">
                 Kayıtlı alan ekleme
               </h1>
@@ -41,11 +41,11 @@ function Dashboard() {
           </div>
         </div>
         <div className="w-[100%] bg-dayComponentBg ">
-          <div className="flex justify-start bg-dayComponentBg pb-8 lg:mx-[9rem] max-[768px]:mx-[2rem]">
+          <div className="flex justify-start bg-dayComponentBg pb-8 lg:mx-[9rem] max-[768px]:mx-[1.5rem]">
             <Agenda />
           </div>
         </div>
-        <div className="bg-dayComponentBg border-stepBorder1 border-2 rounded-xl mx-[9rem] max-[768px]:h-auto  max-[768px]:mx-[1.5rem] max-[768px]:w-[24rem] b-5">
+        <div className="bg-dayComponentBg border-stepBorder1 border-2 rounded-xl mx-[9rem] max-[768px]:h-auto  max-[768px]:mx-auto max-[768px]:w-[23rem] b-5">
           <h1 className="text-buttonColor text-2xl text-center font-semibold mt-3">
             Randevu Talepleri
           </h1>
