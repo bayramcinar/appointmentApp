0901dee3695de90648fa2e9e6ab96f6f4eb63a7f
Bayram Çınar <bayram200212@gmail.com>
2024-02-09 17:47:23 +0300

    update

--- src/components/commonModules/navbar.jsx
+++ src/components/commonModules/navbar.jsx
@@ -4,20 +4,20 @@ import "../../style/navbar.css";
 
 function Navbar() {
   return (
-    <div className="sticky flex items-center justify-center top-0 bg-premiumPurple w-full z-[5] max-w-[100vw]">
+    <div className="sticky flex items-center justify-center top-0 bg-gray-200 text-gray-600 w-full z-[5] max-w-[100vw]">
       <nav>
         <ul className="flex flex-row max-w-[100vw] justify-around">
-          <li className="text-sm text-dayComponentBg font-semibold text-center my-5 mx-2 nav-item">
+          <li className="text-sm font-semibold text-center my-5 mx-2 nav-item">
             <Link className="nav-link relative" to={""}>
               Appointment Area
             </Link>
           </li>
-          <li className="text-sm text-dayComponentBg font-semibold text-center my-5 mx-2 nav-item">
+          <li className="text-sm  font-semibold text-center my-5 mx-2 nav-item">
             <Link className="nav-link relative" to={"dashboard"}>
               Dashboard
             </Link>
           </li>
-          <li className="text-sm text-dayComponentBg font-semibold text-center my-5 mx-2 nav-item">
+          <li className="text-sm  font-semibold text-center my-5 mx-2 nav-item">
             <Link className="nav-link relative" to={"myAppointments"}>
               My Appointments
             </Link>
